c2c631d728162d496ae64149923a7af75ca48815
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-08 06:11:56 -0800

    write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return : always (0) succes
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n");
+	return (0);
+}
+
