74cd765325ef3c7424c544cfe2ea3637881d5da6
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-10 07:19:28 -0800

    print alphabet except

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to print lower case except q,e
+ *
+ * Return:always (0)sucess
+ */
+int main(void)
+{
+	char lc;
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+			putchar(lc);
+	}
+			putchar('\n');
+	return (0);
+}
