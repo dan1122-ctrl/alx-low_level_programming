037800f3363ee06772bb10ba69f03a431d7e9dc6
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-10 02:09:07 -0800

    positive nagative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,13 +16,12 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n == 0)
+	else if (n == 0) 
 	{
 		printf("%d is zero\n", n);
 	}
-		return (0);
+	else
+		printf("%d is negative\n", n);
+
+	return (0);
 }
