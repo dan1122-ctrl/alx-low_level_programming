f1cce02e75a4859058b0378b1df64715fa1d61ab
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-10 03:32:16 -0800

    lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+/* lower case alphabet */
+
+/* al is for lower case alphabet */
+
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+		putchar('\n');
+
+	return (0);
+}
