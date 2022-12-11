4f2bbfad8bdded94e3562986d6bd740a39303ac6
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-11 02:10:59 -0800

    digit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 0; num < 10; num++)
-	{
-		putchar("%d", num);
-		putchar("\n");
-	}
+	for (n = 0; n < 10; n++)
+
+		printf("%d", n);
+
+	printf("\n");
+
 	return (0);
 }
