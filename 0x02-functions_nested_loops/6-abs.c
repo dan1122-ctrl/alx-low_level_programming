1191a8310ba4cb06a486f1ba52f55f3e0bef2245
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-13 04:17:55 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main"
+/**
+ * _abs - to print  absolute value of an intiger
+ * @i: the intiger to be printed
+ * Return: return to i
+ */
+int _abs(int i)
+{
+	if (t >= 0)
+		return (i);
+	else
+		return (-i);
+}
+
