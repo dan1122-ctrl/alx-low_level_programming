c9b7d902e850168bced69bc3bcc75d5c693a24ec
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-12 22:30:42 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - checkout the code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char al = 'a';
+
+	for (al = 'a'; al <= 'z'; al++)
+		_putchar(al);
+	_putchar('\n');
+	return (0);
+
+}
