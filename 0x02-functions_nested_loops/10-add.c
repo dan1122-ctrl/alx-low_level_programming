bbf86619e0e045613ab44f050809afcbdc7196c2
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-14 00:34:13 -0800

    adding intiger

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @i: is the first intiger
+ * @j: is the second intiger
+ * Return: sum of both intigers
+ */
+int add(int i, int j)
+{
+	int i, j;
+
+	return (i + j);
+}
