51432bc31e532e8c3485087df523e7b092eb7952
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-14 10:17:56 -0800

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,6 @@ void print_to_98(int n)
 			printf("%d ", n--);
 
 		printf("%d\n", n);
-		printf(",");
 	}
 		else
 		{
@@ -22,6 +21,5 @@ void print_to_98(int n)
 			printf("%d ", n++);
 
 		printf("%d\n", n);
-		printf(",");
 		}
 }
