53b3341c0e55ff5a584cf249a520f91f2ac61369
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-19 16:36:25 -0800

    half string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@
 void puts_half(char *str)
 {
         int i = 0;
-        for (; str[i] != '/0'; i++)
+        for (; str[i] != '\0'; i++)
         {
                 if (i % 2 == 0)
                         _putchar(str[i]);
