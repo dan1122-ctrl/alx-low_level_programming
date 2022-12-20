8d100f010fcac2e2e4f50e1c15fd1c97a88e2d35
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-19 16:22:52 -0800

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: strig
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	for (; str[i] != '/0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+}
+
+
