b251e8a6fb55409af63fd800ebd45349cd3a9940
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-21 20:39:51 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,17 @@ char *_strcat(char *dest, char *src)
 
 	a = 0;
 
-	while (dest[a] != '/0')
+	while (dest[a] != '\0')
 	{
 		a++;
 	}
 	b = 0;
-	while (src[b] != '/0')
+	while (src[b] != '\0')
 	{
 		dest[a] = src[b];
 		a++;
 		b++;
 	}
-	dest[a] = '/0';
+	dest[a] = '\0';
 	return (dest);
 }
