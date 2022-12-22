94309e8a2f0c7f0b76415be105cb50da20d3eecf
dan1122-ctrl <danieltekle88@gmail.com>
2022-12-21 20:33:22 -0800

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,12 +3,14 @@
  * _strcat - appends the src string to the dest string
  * @dest:destination file
  * @src: source file
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int a, b;
 
 	a = 0;
+
 	while (dest[a] != '/0')
 	{
 		a++;
@@ -17,8 +19,8 @@ char *_strcat(char *dest, char *src)
 	while (src[b] != '/0')
 	{
 		dest[a] = src[b];
-		b++;
 		a++;
+		b++;
 	}
 	dest[a] = '/0';
 	return (dest);
