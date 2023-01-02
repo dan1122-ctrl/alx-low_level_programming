b1982b821eca0c3dcf9141c1a08934bb739ebe54
dan1122-ctrl <danieltekle88@gmail.com>
2023-01-02 03:54:38 -0800

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _memcpy - function copies
- * @dest: the address of memory to print
+ * _memcpy -  function that copies memory area
+ * dest: the address of memory to print
  * @src:  the memory to be print
  * @n:size
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int a;
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
 		*(dest + a) = *(src + a);
